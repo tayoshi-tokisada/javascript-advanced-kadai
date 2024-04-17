8ff86f22bb2ff1fed9584c4fb9aa1b397d119849
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-17 17:56:20 +0900

    change 土台作り

---
+++ kadai_tutorial_output/js/main.js
@@ -0,0 +1,14 @@
+// ランダムなテキストを表示
+const createText = () => {};
+
+// キー入力の判定
+const keyPress = e => {};
+
+// タイピングスキルのランクを判定
+const rankCheck = score => {};
+
+// ゲームを終了
+const gameOver = id => {};
+
+// カウントダウンタイマー
+const timer = () => {};
