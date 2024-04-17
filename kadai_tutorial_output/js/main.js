2da317b0634ab4c154c922777fe909c7b7c84df1
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-17 19:13:31 +0900

    change textList作成

--- kadai_tutorial_output/js/main.js
+++ kadai_tutorial_output/js/main.js
@@ -1,5 +1,21 @@
+// dataの取得
+import data from './json/data.json' assert {type: 'json'};
+
+// 変数宣言
+let untyped;
+
+// HTML要素の取得
+const untypedField = document.getElementById('untyped')
+
 // ランダムなテキストを表示
-const createText = () => {};
+const createText = () => {
+  // 0 ~ textLists.length-1 のランダムな数値を生成する
+  let randomNum = Math.floor(Math.random() * textLists.length);
+  // textListsの内容をテキストに表示
+  untyped = data.textList[randomNum];
+  untypedField.textContent = untyped;
+};
+createText();
 
 // キー入力の判定
 const keyPress = e => {};
